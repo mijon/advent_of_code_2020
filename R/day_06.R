8b013f9f5a364c152a717a1a52d43873b2de6e85
Michael Jones <michael.jones.gsy@gmail.com>
2020-12-08 12:59:19 +0000

    day 6

--- R/day_06.R
+++ R/day_06.R
@@ -1,13 +1,25 @@
 library(tidyverse)
+library(here)
 
 # Part 1
-read_file(here("data", "06_input.txt")) %>% 
+combined_unique <- read_file(here("data", "06_input.txt")) %>% 
   str_split("\\n\\n") %>%
   map(~str_replace_all(.x, "\\n", "")) %>% 
   map(~str_extract_all(.x, ".")) %>% 
   pluck(1) %>% 
-  map(unique) %>% 
+  map(unique)
+
+combined_unique %>% 
   map_dbl(length) %>% 
   sum()
 
 ## Part 2
+groups <- read_file(here("data", "06_input.txt")) %>%
+  str_split("\\n\\n") %>%
+  map(~str_split(.x, "\\n")) %>%
+  pluck(1)
+
+map(groups, ~str_extract_all(.x, ".") %>%
+      reduce(intersect) %>%
+      length()) %>%
+  reduce(sum)
