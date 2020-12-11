61a8585db14911ffbc186cca1e948444d936be54
Michael Jones <michael.jones.gsy@gmail.com>
2020-12-11 13:09:07 +0000

    day 8 part 2

--- R/day_08.R
+++ R/day_08.R
@@ -10,20 +10,28 @@ parse_instruction <- function(instruction) {
        value = parse_number(instruction[2]))
 }
 
-make_history <- function(instruction_set) {
+run_prog <- function(instruction_set) {
+  # Store max instruction number
+  n_instructions <- length(instruction_set)
   
   previous_pointers <- c()
   previous_accumulator <- c()
+  previous_actions <- c()
+  previous_values <- c()
   
   # initialise state
   pointer <- 1
   accumulator <- 0
+  terminates <- FALSE
   
   while (!pointer %in% previous_pointers) {
     previous_pointers <- c(previous_pointers, pointer)
     previous_accumulator <- c(previous_accumulator, accumulator)
     
-    instruction <- parse_instruction(instructions[[pointer]])
+    # instruction <- parse_instruction(instructions[[pointer]])
+    instruction <- instruction_set[[pointer]]
+    previous_actions <- c(previous_actions, instruction$action)
+    previous_values <- c(previous_values, instruction$value)
     
     if (instruction$action == "nop") {
       pointer <- pointer + 1
@@ -35,14 +43,55 @@ make_history <- function(instruction_set) {
     } else {
       stop(paste("unknown instruction:", instruction$action))
     }
+    
+    if (pointer > n_instructions) {
+      # print(glue::glue("reached the end with accumulator value: {accumulator}"))
+      terminates <- TRUE
+      break
+    }
   }
   
-  tibble(pointer = previous_pointers,
-         accumulator = previous_accumulator) 
+  list(
+    path = tibble(pointer = previous_pointers,
+                  accumulator = previous_accumulator,
+                  action = previous_actions,
+                  value = previous_values),
+    terminates = terminates
+  )
 }
 
-instructions <- readLines(here("data", "08_input.txt"))
-make_history(instruction_set = instructions)
+instructions <- readLines(here("data", "08_input.txt")) %>% map(parse_instruction)
+run_prog(instruction_set = instructions) %>% pluck("path") %>%  tail()
 
 # Part 2
+flip_prog <- function(instruction_set, p) {
+  current_action <- instruction_set[[p]]$action
+  
+  if (current_action == "nop") {
+    new_action <- "jmp"
+  } else if (current_action == "jmp") {
+    new_action <- "nop"
+  } else {
+    new_action <- current_action
+  }
+  
+  instruction_set[[p]]$action <- new_action
+  instruction_set
+}
 
+correct_program <- function(instruction_set) {
+  initial_run <- run_prog(instruction_set)
+  path <- initial_run$path$pointer
+  
+  for (p in path) {
+    tmp_prog <- flip_prog(instruction_set, p)
+    tmp_run <- run_prog(tmp_prog)
+    
+    if (tmp_run$terminates) {
+      return(tmp_run)
+    }
+  }
+}
+
+corrected_program <- correct_program(instructions)
+tail(corrected_program$path)
